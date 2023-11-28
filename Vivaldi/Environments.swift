ede2a63948224fb142c2ac100982bb93fa77db54
lili <lilikazine@gmail.com>
2023-11-28 11:27:31 +0800

    auth

---
+++ Vivaldi/Environments.swift
@@ -0,0 +1,12 @@
+//
+//  Environments.swift
+//  Vivaldi
+//
+//  Created by LiLi on 2023/11/28.
+//
+
+import SwiftUI
+
+class LockState: ObservableObject {
+    @Published var isLocked: Bool = true
+}
