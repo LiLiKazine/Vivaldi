c14fb3b46f09966a8df15a338307b20f443595f9
lili <lilikazine@gmail.com>
2024-05-20 22:57:09 +0800

    ImageKit

--- Vivaldi/ImageKit/IKImage.swift
+++ Vivaldi/ImageKit/IKImage.swift
@@ -17,3 +17,31 @@ struct IKImage: View {
 #Preview {
     IKImage()
 }
+
+
+protocol AsyncImageDataSource {
+    func retrive() async throws -> Data
+}
+
+protocol RetrivedDataProcessor {
+    func process(_ data: Data) async throws -> Data
+}
+
+protocol ThumbnailGenerator {
+    func generate(from data: Data) async throws -> Data
+}
+
+protocol ImageMemoryCache {
+    
+    var totalCostLimit: Int { get }
+    var countLimit: Int { get }
+    
+    var onCacheExpire: (Data, String) -> Void { get set }
+    
+    func cache(original data: String, key: String)
+    
+    func cache(thumbnail data: String, key: String)
+    
+}
+
+
