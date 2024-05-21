9b0cceac7ff7eef0278bf6f1c8f6f7b6c1009ece
lili <lilikazine@gmail.com>
2024-05-21 11:44:23 +0800

    IKIMage

--- Vivaldi/ImageKit/IKImage.swift
+++ Vivaldi/ImageKit/IKImage.swift
@@ -9,22 +9,84 @@ import SwiftUI
 
 struct IKImage: View {
     
+    let context: Context
+    
+    init(context: Context) {
+        self.context = context
+    }
+    
+    init(source: Source) {
+        self.init(context: Context(source: source))
+    }
+    
     var body: some View {
         Text("Hello, World!")
+        
+    }
+}
+
+extension IKImage {
+    
+    func thumbnailGenerator(_ generator: ThumbnailGenerator) -> IKImage {
+        context.options.thumbnailGenerator = generator
+        return self
+    }
+    
+    func memoryCache(_ cache: ImageMemoryCache) -> IKImage {
+        context.options.memoryCache = cache
+        return self
+    }
+    
+    func diskCache(_ cache: ImageDiskCache) -> IKImage {
+        context.options.diskCache = cache
+        return self
+    }
+    
+    private func append(_ configuration: @escaping (Image) -> Image) -> IKImage {
+        context.configurations.append(configuration)
+        return self
+    }
+    
+}
+
+extension IKImage {
+    enum Source {
+        case image(Image)
+        case async(AsyncImageDataSource)
+    }
+    
+    class Context {
+        
+        let source: Source
+       
+        var options: Options
+        
+        var configurations: [(Image) -> Image]
+        
+        init(source: Source,
+             options: Options = .init(),
+             configurations: [(Image) -> Image] = []) {
+            self.source = source
+            self.options = options
+            self.configurations = configurations
+        }
+    }
+    
+    struct Options {
+        var thumbnailGenerator: ThumbnailGenerator?
+        var memoryCache: ImageMemoryCache?
+        var diskCache: ImageDiskCache?
     }
 }
 
 #Preview {
-    IKImage()
+    IKImage(source: .image(Image(systemName: "doc.text.image")))
 }
 
 
 protocol AsyncImageDataSource {
+    var cacheKey: String? { get }
     func retrive() async throws -> Data
-}
-
-protocol RetrivedDataProcessor {
-    func process(_ data: Data) async throws -> Data
 }
 
 protocol ThumbnailGenerator {
@@ -37,6 +99,14 @@ protocol ImageMemoryCache {
     var countLimit: Int { get }
     
     var onCacheExpire: (Data, String) -> Void { get set }
+    
+    func cache(original data: String, key: String)
+    
+    func cache(thumbnail data: String, key: String)
+    
+}
+
+protocol ImageDiskCache {
     
     func cache(original data: String, key: String)
     
