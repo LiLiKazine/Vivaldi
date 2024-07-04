b513abdae3982c7ca6d818aa158781a6766fbb70
lili <lilikazine@gmail.com>
2024-07-04 16:24:21 +0800

    Revert "?"
    This reverts commit 6c8cd19dbfd96d53fea968da176001b01b5ef334.

--- Vivaldi/VivaldiApp.swift
+++ Vivaldi/VivaldiApp.swift
@@ -7,92 +7,17 @@
 
 import SwiftUI
 import SwiftData
-import Combine
 
 @main
 struct VivaldiApp: App {
     
     @State private var uiConfiguration = UIConfiguration.shared
     
-    
-    @State var text = "sdasdasdas"
-    @State var color = UIColor.green
     var body: some Scene {
         WindowGroup {
-            VStack {
-                Test()
-                    .text(text)
-                    .color(color)
-                    .frame(height: 400)
-                    .border(.red)
-                
-                Button("Random") {
-                    text = "text updated!"
-                    color = UIColor.red
-                }
-            }
+            ContentView()
         }
         .environmentObject(LockState())
         .environment(uiConfiguration)
-    }
-}
-
-class Observe: ObservableObject {
-    @Published var text: String?
-    @Published var textColor: UIColor?
-}
-
-struct Test: UIViewRepresentable {
-    
-    class Coordinator {
-        weak var label: UILabel?
-        
-        var cancellables = Set<AnyCancellable>()
-        
-        func subscribe(_ obj: Observe) {
-            obj.$text.sink { [weak self] text in
-                self?.label?.text = text
-            }
-            .store(in: &cancellables)
-            obj.$textColor.sink { [weak self] color in
-                self?.label?.textColor = color
-            }
-            .store(in: &cancellables)
-        }
-        
-    }
-    
-    static var obj = Observe()
-    
-    func makeUIView(context: Context) -> UILabel {
-        let label = UILabel()
-        context.coordinator.label = label
-        
-        context.coordinator.subscribe(Self.obj)
-        return label
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        let coordinator = Coordinator()
-        return coordinator
-    }
-    
-    func updateUIView(_ uiView: UILabel, context: Context) {
-        
-    }
-    
-}
-
-
-extension Test {
-    
-    func text(_ text: String) -> Self {
-        Self.obj.text = text
-        return self
-    }
-    
-    func color(_ color: UIColor) -> Self {
-        Self.obj.textColor = color
-        return self
     }
 }
